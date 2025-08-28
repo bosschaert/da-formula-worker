d905508dbda93b75ed702839c64714c6be1ddd9c
David Bosschaert <bosschae@adobe.com>
2025-08-28 09:16:12 +0100

    Initial unit tests

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
+
+/*
+ * Copyright 2025 Adobe. All rights reserved.
+ * This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License. You may obtain a copy
+ * of the License at http://www.apache.org/licenses/LICENSE-2.0
  *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ * OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
  */
 
-function sort(field, numSort, json) {
+export function sort(field, numSort, json) {
 	console.log('Sorting on', field, '- numsort', numSort);
 	const data = json.data;
 	if (numSort) {
--- test/index.spec.js
+++ test/index.spec.js
@@ -1,8 +1,44 @@
-// Boilerplate test code, write real tests instead
+/*
+ * Copyright 2025 Adobe. All rights reserved.
+ * This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License. You may obtain a copy
+ * of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ * OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
 
-// import { env, createExecutionContext, waitOnExecutionContext, SELF } from 'cloudflare:test';
 // import { describe, it, expect } from 'vitest';
-// import worker from '../src';
+import assert from 'assert';
+import { sort } from '../src/index.js';
+
+describe('Sheets Worker', () => {
+	it('sorts json sheets', () => {
+		const json = {
+			"data": [
+				{ "name": "Fred", "age": "010" },
+				{ "name": "Wilma", "age": "8" },
+				{ "name": "Barney", "age": "30" },
+				{ "name": "Betty", "age": "28" }
+			]
+		};
+		const sorted = sort('name', false, json);
+		assert.equal(sorted.data[0].name, 'Barney');
+		assert.equal(sorted.data[1].name, 'Betty');
+		assert.equal(sorted.data[2].name, 'Fred');
+		assert.equal(sorted.data[3].name, 'Wilma');
+
+		const sorted2 = sort('age', true, json);
+		assert.equal(sorted2.data[0].name, 'Wilma');
+		assert.equal(sorted2.data[1].name, 'Fred');
+		assert.equal(sorted2.data[2].name, 'Betty');
+		assert.equal(sorted2.data[3].name, 'Barney');
+	});
+
+	// it('')
+});
 
 // describe('Hello World worker', () => {
 // 	it('responds with Hello World! (unit style)', async () => {
